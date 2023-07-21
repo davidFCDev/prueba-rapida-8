aede8274b601297f9b62d5221ce9027498d85b32
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-21 22:35:23 +0200

    hook useFilter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import "./App.css";
 import { useState } from "react";
 import Products from "./components/Products";
+import { Footer } from "./components/Footer";
 import { products as initialProducts } from "./mocks/products.json";
+import { Header } from "./components/Header";
+
+function useFilters() {
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0,
+  });
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+  };
+
+  return { setFilters, filterProducts };
+}
 
 function App() {
   const [products] = useState(initialProducts);
 
+  const { setFilters, filterProducts } = useFilters();
+
+  const filteredProducts = filterProducts(products);
+
   return (
     <div>
-      <Products products={products} />
+      <Header />
+      <Products products={filteredProducts} />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Filters.jsx
@@ -0,0 +1,13 @@
+import "./Filters.css";
+
+function Filters({ onChange }) {
+  const [minPrice, setMinPrice] = useState(0);
+  const minPriceFilterId = useId();
+  const categoryFilterId = useId();
+
+  const handle
+
+  return <div>Filters</div>;
+}
+
+export default Filters;
