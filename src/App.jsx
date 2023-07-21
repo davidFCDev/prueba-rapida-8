4b4317d6db25344cfca912e3ad27038062b2e940
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-21 22:43:26 +0200

    sacamos hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,20 @@ import Products from "./components/Products";
 import { Footer } from "./components/Footer";
 import { products as initialProducts } from "./mocks/products.json";
 import { Header } from "./components/Header";
-
-function useFilters() {
-  const [filters, setFilters] = useState({
-    category: "all",
-    minPrice: 0,
-  });
-
-  const filterProducts = (products) => {
-    return products.filter((product) => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category === "all" || product.category === filters.category)
-      );
-    });
-  };
-
-  return { setFilters, filterProducts };
-}
+import { useFilters } from "./hooks/useFilters";
 
 function App() {
   const [products] = useState(initialProducts);
 
-  const { setFilters, filterProducts } = useFilters();
+  const { filters, filterProducts, setFilters } = useFilters();
 
   const filteredProducts = filterProducts(products);
 
   return (
     <div>
-      <Header />
+      <Header changeFilters={setFilters} />
       <Products products={filteredProducts} />
-      <Footer />
+      <Footer filters={filters} />
     </div>
   );
 }
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,3 +1,4 @@
+import { useId, useState } from "react";
 import "./Filters.css";
 
 function Filters({ onChange }) {
@@ -5,9 +6,45 @@ function Filters({ onChange }) {
   const minPriceFilterId = useId();
   const categoryFilterId = useId();
 
-  const handle
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value);
+    onChange((prevState) => ({
+      ...prevState,
+      minPrice: event.target.value,
+    }));
+  };
 
-  return <div>Filters</div>;
+  const handleChangeCategory = (event) => {
+    onChange((prevState) => ({
+      ...prevState,
+      category: event.target.value,
+    }));
+  };
+
+  return (
+    <section className="filters">
+      <div>
+        <label htmlFor={minPriceFilterId}>Price</label>
+        <input
+          type="range"
+          id={minPriceFilterId}
+          min="0"
+          max="1000"
+          onChange={handleChangeMinPrice}
+        />
+        <span>{minPrice}</span>
+      </div>
+
+      <div>
+        <label htmlFor={categoryFilterId}>Category</label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
+          <option value="all">All</option>
+          <option value="laptops">Laptops</option>
+          <option value="smartphones">Smartphones</option>
+        </select>
+      </div>
+    </section>
+  );
 }
 
 export default Filters;
